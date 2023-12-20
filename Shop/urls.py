49fefc79639c7a5642df9c5c298ea63f6f27adde
Miguelangel <misoto36@hotmail.com>
2023-12-20 16:13:47 -0500

    Add shop cart

--- Shop/urls.py
+++ Shop/urls.py
@@ -6,5 +6,10 @@ app_name = 'shop'
 urlpatterns = [ 
     path('',index, name='index'),
     path('product/<int:pk>/', product_detail, name='product_detail'),
-    path('product/create', product_create, name='product_create')
+    path('product/create/', product_create, name='product_create'),
+    #Shopping Carts URLs
+    path('cart/', cart_detail , name='cart-detail'),
+    path('cart/add/<int:pk>/', cart_add, name='cart-add'),
+    path('cart/remove/<int:pk>/', cart_remove, name='cart-remove'),
+    path('cart/update/<int:pk>/', cart_update, name='cart-update'),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
+
 from .models import * 
+from .utils import get_cart
 
 def index(request):
     categories = Category.objects.all()
@@ -9,7 +11,6 @@ def index(request):
         'categories': categories,
         'products' : products
     }
-
     return render(request, 'shop/index.html', context)
 
 def product_detail(request, pk):
@@ -36,3 +37,40 @@ def product_create(request):
     context = {
         'categories':Category.objects.all()}
     return render(request, 'shop/product_create.html', context)
+
+
+
+#Shopping Cart Views
+
+def cart_detail(request):
+    cart = get_cart(request)
+    context = {
+        'cart':cart
+    }
+    return render(request,'shop/detail.html', context)
+
+def cart_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    cart = get_cart(request)
+    quantity = request.POST.get('quantity')
+    cart_item, created = CartItem.objects.get_or_create(
+        cart = cart,
+        product = product,
+        defaults={'quantity':quantity}
+    )
+    if not created:
+        cart_item.quantity += int(quantity)
+        cart_item.save()
+    return redirect('shop:cart-detail')   
+
+def cart_remove(request, pk):
+    cart_item = get_object_or_404(CartItem, pk=pk)
+    cart_item.delete()
+    return redirect('shop:cart-detail')
+
+def cart_update(request, pk):
+    cart_item =  get_object_or_404(CartItem, pk = pk)
+    quiantity = request.POST.get('quantity')
+    cart_item.quantity = quiantity
+    cart_item.save()
+    return redirect('shop:cart-detail')
