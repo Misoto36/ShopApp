c286cfa63b52e2d9b98d3b374198e34c0bc1502a
Miguelangel <misoto36@hotmail.com>
2023-12-20 10:12:03 -0500

    Agrgando Proyecto

---
+++ Shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'shop'
+
+urlpatterns = [ 
+    path('',index, name='index'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('product/create', product_create, name='product_create')
+]
---
+++ Shop/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from .models import * 
+
+def index(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context = {
+        'categories': categories,
+        'products' : products
+    }
+
+    return render(request, 'shop/index.html', context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request,'shop/product_detail.html', context)
+
+def product_create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category = request.POST.get('category')
+        category = Category.objects.get(id = category)
+        product = Product.objects.create(
+            name = name,
+            description = description,
+            price = price,
+            category= category
+        )
+        return redirect('shop:product_detail', pk=product.id )
+    context = {
+        'categories':Category.objects.all()}
+    return render(request, 'shop/product_create.html', context)
